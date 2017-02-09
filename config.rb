ef5864403ad1702b74bb0b38ab1a7e526a8ac35b
Niko Matses <nikolas.matses@gmail.com>
2017-02-09 16:54:18 -0500

    sass subfolders, homepage finessing

--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ http_path = "/"
 css_dir = "app/assets/css"
 sass_dir = "sass"
 images_dir = "app/assets/img"
-javascripts_dir = "app/app/assets/js"
+javascripts_dir = "app/assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
